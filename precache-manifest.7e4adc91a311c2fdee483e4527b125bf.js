d3515cdf8c7a87d0fef2bfae9fab094abf840fb0
Ayomipo Solaja <ayomiposolaja@gmail.com>
2021-05-07 06:35:32 +0100

    Updates

---
+++ precache-manifest.7e4adc91a311c2fdee483e4527b125bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aa8c1ab830dea2b450adcd4ff793952",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b54e1a0e3252ed38f2e5",
+    "url": "/monsters-rolodex/static/css/main.ac03ac47.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b54e1a0e3252ed38f2e5",
+    "url": "/monsters-rolodex/static/js/main.bd9632ea.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
